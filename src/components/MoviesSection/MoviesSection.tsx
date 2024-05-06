4caa5bf00738ab969e409eec1d369229646e607f
Pavel Didenko <patriot2006_06@mail.ru>
2024-05-06 12:08:43 +0300

    feat: implement temporary page filters reset using page reload

--- src/components/MoviesSection/MoviesSection.tsx
+++ src/components/MoviesSection/MoviesSection.tsx
@@ -61,29 +61,13 @@ export function MoviesSection({searchParams}: searchPageParams) {
           filter="primary_release_year"
         />
         <RatingInputs searchParams={searchParams} />
-        {/* <Link
+        <Link
           className={style.resetFilters}
-          href="#"
-          onClick={() => {
-            searchParams = {};
-            router.push('./')
-            console.log(searchParams)
-            // setTimeout(() => window.location.reload(), 200);
-          }}
+          href="/"
+          onClick={() => setTimeout(() => window.location.reload(), 200)}
         >
           Reset filters
-        </Link> */}
-        <Box
-          className={style.resetFilters}
-          onClick={() => {
-            searchParams = {};
-            router.push('/')
-            console.log(searchParams)
-            // setTimeout(() => window.location.reload(), 200);
-          }}
-        >
-          Reset filters
-        </Box>
+        </Link>
       </Flex>
       <Box className={style.sortInputWrapper}>
         <Dropdown
