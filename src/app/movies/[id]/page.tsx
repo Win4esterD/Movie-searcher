3a3b46a4690624bbc91629278f1df65070201175
Pavel Didenko <patriot2006_06@mail.ru>
2024-05-14 16:18:56 +0300

    feat: implement 404 page

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -7,6 +7,7 @@ import {getMovieTrailer} from '@/utils';
 import {logoBaseURL} from '@/utils/urls';
 import Image from 'next/image';
 import {movie} from '@/types/movie';
+import {MoviesLayout} from '@/layouts';
 
 export default async function Movie({params}: {params: {id: string}}) {
   let link;
@@ -23,73 +24,75 @@ export default async function Movie({params}: {params: {id: string}}) {
   const trailer = getMovieTrailer(videos);
 
   return (
-    <Box className={style.right}>
-      <Box className={style.contentContainer}>
-        <Text size="sm" c="var(--main-purple)" className={style.movieName}>
-          {movieInfo?.title}
-        </Text>
-        <MovieBlock
-          poster={movieInfo?.poster_path}
-          title={movieInfo?.title}
-          releaseDate={movieInfo?.release_date}
-          rating={movieInfo?.vote_average}
-          votes={movieInfo?.vote_count}
-          time={movieInfo?.runtime}
-          budget={movieInfo?.budget}
-          revenue={movieInfo?.revenue}
-          genres={movieInfo?.genres}
-          id={params?.id}
-        />
+    <MoviesLayout>
+      <Box className={style.right}>
+        <Box className={style.contentContainer}>
+          <Text size="sm" c="var(--main-purple)" className={style.movieName}>
+            {movieInfo?.title}
+          </Text>
+          <MovieBlock
+            poster={movieInfo?.poster_path}
+            title={movieInfo?.title}
+            releaseDate={movieInfo?.release_date}
+            rating={movieInfo?.vote_average}
+            votes={movieInfo?.vote_count}
+            time={movieInfo?.runtime}
+            budget={movieInfo?.budget}
+            revenue={movieInfo?.revenue}
+            genres={movieInfo?.genres}
+            id={params?.id}
+          />
 
-        <Box className={style.descriptionBlock}>
-          <Box className={style.descriptionBlockContent}>
-            <Text fw={700} size="xl" className={style.trailer}>
-              Trailer
-            </Text>
-            <YouTubeFrame
-              youtubeKey={trailer ? trailer : ''}
-              css={{position: 'relative', left: '0.3rem'}}
-            />
-            <Divider
-              color="var(--alt-grey)"
-              className={`${style.dividerIdent} ${style.divider}`}
-            />
-            <Text fw={700} size="xl" className={style.descriptionCaption}>
-              Description
-            </Text>
-            <Text className={style.overview}>{movieInfo?.overview}</Text>
-            <Divider
-              color="var(--alt-grey)"
-              className={`${style.dividerIdent} ${style.divider}`}
-            />
-            <Text fw={700} size="xl" className={style.productionCaption}>
-              Production
-            </Text>
-            <Flex
-              direction="column"
-              gap="0.75rem"
-              className={style.productCompaniesWrapper}
-            >
-              {movieInfo?.production_companies &&
-                movieInfo.production_companies.map(item => {
-                  return (
-                    <Flex gap="0.5rem">
-                      <Image
-                        src={logoBaseURL + item.logo_path}
-                        alt={item.name}
-                        width={40}
-                        height={40}
-                      />
-                      <Text fw={700} className={style.productionCompany}>
-                        {item.name}
-                      </Text>
-                    </Flex>
-                  );
-                })}
-            </Flex>
+          <Box className={style.descriptionBlock}>
+            <Box className={style.descriptionBlockContent}>
+              <Text fw={700} size="xl" className={style.trailer}>
+                Trailer
+              </Text>
+              <YouTubeFrame
+                youtubeKey={trailer ? trailer : ''}
+                css={{position: 'relative', left: '0.3rem'}}
+              />
+              <Divider
+                color="var(--alt-grey)"
+                className={`${style.dividerIdent} ${style.divider}`}
+              />
+              <Text fw={700} size="xl" className={style.descriptionCaption}>
+                Description
+              </Text>
+              <Text className={style.overview}>{movieInfo?.overview}</Text>
+              <Divider
+                color="var(--alt-grey)"
+                className={`${style.dividerIdent} ${style.divider}`}
+              />
+              <Text fw={700} size="xl" className={style.productionCaption}>
+                Production
+              </Text>
+              <Flex
+                direction="column"
+                gap="0.75rem"
+                className={style.productCompaniesWrapper}
+              >
+                {movieInfo?.production_companies &&
+                  movieInfo.production_companies.map(item => {
+                    return (
+                      <Flex gap="0.5rem">
+                        <Image
+                          src={logoBaseURL + item.logo_path}
+                          alt={item.name}
+                          width={40}
+                          height={40}
+                        />
+                        <Text fw={700} className={style.productionCompany}>
+                          {item.name}
+                        </Text>
+                      </Flex>
+                    );
+                  })}
+              </Flex>
+            </Box>
           </Box>
         </Box>
       </Box>
-    </Box>
+    </MoviesLayout>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,14 @@ import {MoviesSection} from '@/components';
 import {searchPageParams} from '@/types/searchPage';
 import {Box} from '@mantine/core';
 import style from './page.module.css';
+import {MoviesLayout} from '@/layouts';
 
 export default function Home({searchParams}: searchPageParams): JSX.Element {
   return (
-    <Box component="section" className={style.right}>
-      <MoviesSection searchParams={searchParams} />
-    </Box>
+    <MoviesLayout>
+      <Box component="section" className={style.right}>
+        <MoviesSection searchParams={searchParams} />
+      </Box>
+    </MoviesLayout>
   );
 }
