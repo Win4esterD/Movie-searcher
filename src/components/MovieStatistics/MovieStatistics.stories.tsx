afc486e8bb6bc1e96c276ba126b0be142977af5c
Pavel Didenko <patriot2006_06@mail.ru>
2026-08-21 03:57:10 +0300

    feat: MovieStatistics story

---
+++ src/components/MovieStatistics/MovieStatistics.stories.tsx
@@ -0,0 +1,48 @@
+import { StoryObj, Meta } from '@storybook/nextjs-vite';
+import { MovieStatistics } from './MovieStatistics';
+
+const meta = {
+  component: MovieStatistics,
+} satisfies Meta<typeof MovieStatistics>;
+
+export default meta;
+
+type Story = StoryObj<typeof MovieStatistics>;
+
+export const Primary = {
+  args: {
+    rating: 9,
+    votes: 15,
+    styles: {
+      marginTop: '1rem',
+    },
+  },
+} satisfies Story;
+
+export const NoStyles = {
+  args: {
+    ...Primary.args,
+    styles: undefined,
+  },
+} satisfies Story;
+
+export const NoRating = {
+  args: {
+    ...Primary.args,
+    rating: undefined,
+  },
+} satisfies Story;
+
+export const NoVotes = {
+  args: {
+    ...Primary.args,
+    votes: undefined,
+  },
+} satisfies Story;
+
+export const HugeAmountOfVotes = {
+  args: {
+    ...Primary.args,
+    votes: 45234523452345,
+  },
+} satisfies Story;
